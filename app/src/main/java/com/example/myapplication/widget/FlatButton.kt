f8009d3a6c9d2d123ae91d89431b1e8ccce515b1
HieuTV <hieutv@hybrid-technologies.co.jp>
2020-10-05 08:53:09 +0700

    - refactor name button

--- app/src/main/java/com/example/myapplication/widget/FlatButton.kt
+++ app/src/main/java/com/example/myapplication/widget/FlatButton.kt
@@ -6,8 +6,9 @@ import com.example.myapplication.R
 import com.example.myapplication.ext.dp
 import com.google.android.material.button.MaterialButton
 
-class FlatButton @JvmOverloads constructor (context: Context, attributes: AttributeSet? = null, defStyleAttr: Int = R.attr.materialButtonOutlinedStyle) : MaterialButton(context, attributes, defStyleAttr) {
+class FlatButton @JvmOverloads constructor (context: Context, attributes: AttributeSet? = null, defStyleAttr: Int = R.attr.borderlessButtonStyle) : MaterialButton(context, attributes, defStyleAttr) {
     init {
         cornerRadius = 20.dp
+
     }
 }
--- app/src/main/java/com/example/myapplication/widget/OutlineButton.kt
+++ app/src/main/java/com/example/myapplication/widget/OutlineButton.kt
@@ -6,9 +6,8 @@ import com.example.myapplication.R
 import com.example.myapplication.ext.dp
 import com.google.android.material.button.MaterialButton
 
-class TextButton @JvmOverloads constructor (context: Context, attributes: AttributeSet? = null, defStyleAttr: Int = R.attr.borderlessButtonStyle) : MaterialButton(context, attributes, defStyleAttr) {
+class OutlineButton @JvmOverloads constructor (context: Context, attributes: AttributeSet? = null, defStyleAttr: Int = R.attr.materialButtonOutlinedStyle) : MaterialButton(context, attributes, defStyleAttr) {
     init {
         cornerRadius = 20.dp
-
     }
 }
