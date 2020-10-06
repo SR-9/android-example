77b17df4a8cd6231c75211253aa1d72fa0db6c18
HieuTV <hieutv@hybrid-technologies.co.jp>
2020-10-06 15:48:21 +0700

    - refactor okhttp3

--- app/src/main/java/com/example/myapplication/di/NetworkModule.kt
+++ app/src/main/java/com/example/myapplication/di/NetworkModule.kt
@@ -1,35 +1,32 @@
 package com.example.myapplication.di
 
-import com.example.myapplication.di.annotation.AuthInterceptorOkHttpClient
-import com.example.myapplication.di.annotation.OtherInterceptorOkHttpClient
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ApplicationComponent
 import okhttp3.Interceptor
+import okhttp3.JavaNetCookieJar
 import okhttp3.OkHttpClient
-import javax.inject.Singleton
+import java.net.CookieManager
+import java.util.concurrent.TimeUnit
 
 @Module
 @InstallIn(ApplicationComponent::class)
 object NetworkModule {
-    @AuthInterceptorOkHttpClient
+
     @Provides
     fun provideAuthInterceptorOkHttpClient(
         authInterceptor: Interceptor
     ): OkHttpClient {
         return OkHttpClient.Builder()
+            .apply {
+                connectTimeout(60, TimeUnit.SECONDS)
+                readTimeout(60, TimeUnit.SECONDS)
+                writeTimeout(60, TimeUnit.SECONDS)
+                cookieJar(JavaNetCookieJar(CookieManager.getDefault()))
+            }
             .addInterceptor(authInterceptor)
             .build()
     }
 
-    @OtherInterceptorOkHttpClient
-    @Provides
-    fun provideOtherInterceptorOkHttpClient(
-        otherInterceptor: Interceptor
-    ): OkHttpClient {
-        return OkHttpClient.Builder()
-            .addInterceptor(otherInterceptor)
-            .build()
-    }
 }
