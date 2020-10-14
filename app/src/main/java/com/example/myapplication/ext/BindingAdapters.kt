2f3cc7a51d1ed09c35e0d5548171a54efb33e389
HieuTV <hieutv@hybrid-technologies.co.jp>
2020-10-14 11:39:02 +0700

    - custom data binding

---
+++ app/src/main/java/com/example/myapplication/ext/BindingAdapters.kt
@@ -0,0 +1,32 @@
+package com.example.myapplication.ext
+
+import android.view.View
+import android.widget.Button
+import kotlinx.coroutines.*
+
+//https://medium.com/swlh/android-click-debounce-80b3f2e638f3
+//@BindingAdapter("android:onClick")
+fun setDebouncedListener(view: Button, onClickListener: View.OnClickListener) {
+    val clickWithDebounce: (view: View) -> Unit = debounce(scope = MainScope()) {
+        onClickListener.onClick(it)
+    }
+    view.setOnClickListener(clickWithDebounce)
+}
+
+
+fun <T> debounce(
+    delayMillis: Long = 500L,
+    scope: CoroutineScope,
+    action: (T) -> Unit
+): (T) -> Unit {
+    var debounceJob: Job? = null
+    return { param: T ->
+        if (debounceJob == null) {
+            debounceJob = scope.launch {
+                action(param)
+                delay(delayMillis)
+                debounceJob = null
+            }
+        }
+    }
+}
