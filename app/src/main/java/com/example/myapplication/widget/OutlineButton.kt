7ffeaeb4ff32e4e934b013e54d52c0f43c0fcffa
HieuTV <hieutv@hybrid-technologies.co.jp>
2020-10-14 15:54:09 +0700

    - change style button

--- app/src/main/java/com/example/myapplication/widget/OutlineButton.kt
+++ app/src/main/java/com/example/myapplication/widget/OutlineButton.kt
@@ -3,11 +3,8 @@ package com.example.myapplication.widget
 import android.content.Context
 import android.util.AttributeSet
 import com.example.myapplication.R
-import com.example.myapplication.ext.dp
 import com.google.android.material.button.MaterialButton
 
 class OutlineButton @JvmOverloads constructor (context: Context, attributes: AttributeSet? = null, defStyleAttr: Int = R.attr.materialButtonOutlinedStyle) : MaterialButton(context, attributes, defStyleAttr) {
-    init {
-        cornerRadius = 20.dp
-    }
+
 }
